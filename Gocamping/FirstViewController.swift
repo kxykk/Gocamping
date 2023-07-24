a1af2d789a82b0b0a5a6c034df6ab4fde5349b92
Kang <theman19970919@gmail.com>
2023-07-24 16:04:34 +0800

    Initial Commit

---
+++ Gocamping/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gocamping
+//
+//  Created by 康 on 2023/7/24.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
